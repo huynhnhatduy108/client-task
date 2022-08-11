524f046848836e0f076accf1b4ec282bbd5ea038
Huynh Nhat Duy <huynhnhatduy2112@gmail.com>
2022-08-11 21:29:55 +0700

    fix

--- src/contants.js
+++ src/contants.js
@@ -1,2 +1,2 @@
 export const CATEGORY_TASK = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
-export const URL_SERVER = "http://localhost:5000"
+export const URL_SERVER = "https://green-yellow-server.herokuapp.com"
